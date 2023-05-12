df40d7f4393f92dddcf56b13dd1ac700df550edb
Nguyen Tu <nguyentu00005@gmail.com>
2023-05-12 22:09:35 +0700

    first commit

--- Test1/src/laptrinh_java/HelloJava.java
+++ Test1/src/laptrinh_java/HelloJava.java
@@ -3,6 +3,7 @@ package laptrinh_java;
 public class HelloJava {
     public static void main(String[] agrs) {
         System.out.println("xin chao Java");
+        System.out.println("chao tu");
         
     }
     
